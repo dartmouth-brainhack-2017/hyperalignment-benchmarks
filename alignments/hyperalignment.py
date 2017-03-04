e1f908463b47203f9cb716951dc920b0224e9105
Swaroop Guntupalli <swaroopgj@gmail.com>
2017-03-03 20:28:27 -0500

    ENH & NF: Added inits for imports and added basic hyperalignment and loading functionality

---
+++ alignments/hyperalignment.py
@@ -0,0 +1,38 @@
+from mvpa2.algorithms.hyperalignment import Hyperalignment
+from dataio.hdf5_data import load_data, save_data
+
+def hyperalignment(input_data, output_data, mask, output_suffix, training_runs,
+                   testing_runs, **kwargs):
+    """
+
+    Parameters
+    ----------
+    input_data: Input data path.
+    output_data: Output data path.
+    mask: Path to mask file.
+    output_suffix: Filename suffix for saving aligned data.
+    training_runs: List of runs to be used for training.
+    testing_runs: List of runs to be used for testing.
+    kwargs: Passed onto Hyperalignment
+
+    Returns
+    -------
+    Nothing
+    """
+    # XXX TODO Use mask to load from nifti file
+    dss_train = load_data(input_data, training_runs) #, mask)
+    dss_test = load_data(input_data, testing_runs) #, mask)
+    # Initialize hyperalignment
+    ha = Hyperalignment(**kwargs)
+    # Run hyperalignment on training data
+    hmappers = ha(dss_train)
+    # Align and save data
+    dss_aligned = {}
+    for split, dss in (('train', dss_train),
+                       ('test', dss_test)):
+        dss_hyper = [hm.forward(sd) for hm, sd in zip(hmappers, dss)]
+        if output_data is not None:
+            save_data(dss_hyper, output_suffix+split)
+        dss_aligned[split] = dss_hyper
+
+    return dss_aligned
---
+++ dataio/hdf5_data.py
@@ -0,0 +1,39 @@
+from mvpa2.base.hdf5 import h5save, h5load
+import numpy as np
+import os
+
+
+def load_data(data_path, runs=None):
+    """
+
+    Parameters
+    ----------
+    data_path: Path to data.
+    runs: If not None, which chunks/runs to keep.
+
+    Returns
+    -------
+    List of datasets.
+    """
+    if not os.path.exists(data_path):
+        raise ValueError("Data path doesn't exist: {0}".format(data_path))
+
+    dss = h5load(data_path)
+    if not isinstance(dss, (list, tuple, np.ndarray)):
+        raise TypeError("Input datasets should be a sequence "
+                        "(of type list, tuple, or ndarray) of datasets.")
+
+    if runs is None:
+        return dss
+    else:
+        dss = [sd.select(sadict={'chunks': runs}) for sd in dss]
+        return dss
+
+def save_data():
+    """
+
+    Returns
+    -------
+
+    """
+    pass
