b8eb410bbad80b6ec005a459b6eb7d8fe95610da
Swaroop Guntupalli <swaroopgj@gmail.com>
2017-03-05 16:14:16 -0500

    NF: Smoke  test with pytest

---
+++ tests/test_basics.py
@@ -0,0 +1,33 @@
+import pytest
+from hyperbench.alignments.hyperalignment import hyperalignment
+from hyperbench.alignments.srm import srm_alignment
+from hyperbench.benchmarks.intersubject_correlation import \
+    intersubject_correlation
+from hyperbench.benchmarks.timesegmentclassification import \
+    timesegments_classification
+
+@pytest.mark.parametrize("alignment", ['hyper', 'srm'])
+@pytest.mark.parametrize("normalization", ['zscore', 'percent_signal_change', 'demean'])
+@pytest.mark.parametrize("benchmark", ['isc', 'clf'])
+def test_basics(alignment, normalization, benchmark):
+    data_path = './data/hyperalignment_tutorial_data_2.4_slim.hdf5.gz'
+    # Smoke tests for now
+    if alignment == 'hyper':
+        dss_aligned = hyperalignment(data_path, None, None, None, range(4),
+                                     range(5, 8), normalization=normalization)
+    elif alignment == 'srm':
+        dss_aligned = srm_alignment(data_path, 10, None, None, None, range(4),
+                                     range(5, 8), normalization=normalization)
+    else:
+        pass
+
+    if benchmark == 'isc':
+        corrs_train = intersubject_correlation(dss_aligned['train'])
+        corrs_test = intersubject_correlation(dss_aligned['test'])
+        print("Training mean:", corrs_train.samples.mean())
+        print("Test mean:", corrs_test.samples.mean())
+    elif benchmark == 'clf':
+        errors_train = timesegments_classification(dss_aligned['train'], do_zscore=True)
+        errors_test = timesegments_classification(dss_aligned['test'], do_zscore=True)
+        print("Training error:", errors_train.mean())
+        print("Test error:", errors_test.mean())
